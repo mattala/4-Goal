ce1a9e28ba1774545a105d3214d0871c33ea6eaa
mo.atalla96 <mo.atalla96@gmail.com>
2019-04-20 22:31:03 +0200

    Added Auth Directory

--- private/path_finder.php
+++ private/path_finder.php
@@ -7,12 +7,23 @@
 define("PRIVATE_PATH", __DIR__);
 
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
-
+// root/models
 define("MODELS_PATH", PROJECT_PATH . '/models');
-
+// root/app
 define("APP_PATH", PROJECT_PATH . '/app');
 
+// root/resources
+define("PUBLIC_PATH", PROJECT_PATH . '/public');
+
+// root/public/assets
+define("ASSETS_PATH", PUBLIC_PATH . '/assets');
+
+// root/public/style
+define("STYLE_PATH", PUBLIC_PATH . '/style');
+
+// root/public/js
+define("JS_PATH", PUBLIC_PATH . '/js');
 
 
-// WWW/private/shared
+// root/private/shared
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
